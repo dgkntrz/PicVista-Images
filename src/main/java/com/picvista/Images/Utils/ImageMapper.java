30cd041d2997b9fb642e35e0758f9f54691d98da
Doğukan Terzi <dogukanterzi@gmail.com>
2024-01-11 05:00:10 +0300

    fix date

--- src/main/java/com/picvista/Images/Utils/ImageMapper.java
+++ src/main/java/com/picvista/Images/Utils/ImageMapper.java
@@ -4,15 +4,21 @@ import com.picvista.Images.Model.Image;
 import com.picvista.Images.Model.ImageDTO;
 import org.bson.BsonBinarySubType;
 import org.bson.types.Binary;
+
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.io.IOException;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.Locale;
 
 public class ImageMapper {
 
     public static Image mapImageDTOtoImage(ImageDTO imageDTO) throws IOException {
+        String pattern = "dd/MM/yyyy HH:mm";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern, new Locale("en", "GB"));
+        String date = simpleDateFormat.format(new Date());
         return new Image(imageDTO.getTitle(), imageDTO.getDescription(), imageDTO.getAuthor(), new Binary(BsonBinarySubType.BINARY,
-                imageDTO.getImage().getBytes()), LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS).toString().replace("T", " "));
+                imageDTO.getImage().getBytes()), date);
     }
 }
